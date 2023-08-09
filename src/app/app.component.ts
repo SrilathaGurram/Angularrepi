8561776216660b0394887a395b2f355baa92b94c
SrilathaGurram <srilathagurram545@gmail.com>
2023-08-09 09:24:11 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularcode';
+
+  constructor(private router: Router){
+
+  }
+  login()
+  {
+    this.router.navigate(['/login']);
+
+  }
+  dashboard()
+  {
+    this.router.navigate(['/dashboard']);
+  }
+}
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  Data=[
+    
+      {language:'csharp' ,discover:'AndersHejlsberg'},
+      {language:'python', discover:'GuidoVanRossum'},
+      {language:'c', discover:'DennisRitchie'},
+      {language:'java', discover:'JamesGosling'}
+      
+    
+]
+
+}
